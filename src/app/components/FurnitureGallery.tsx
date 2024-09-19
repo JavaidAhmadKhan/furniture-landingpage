d1394a28fe0e5d0b3492cb9dff79f9beb39c0dfc
Your Name <relaxjavaid@gmail.com>
2024-09-19 15:40:39 +0530

    fixed ts issues now

--- src/app/components/FurnitureGallery.tsx
+++ src/app/components/FurnitureGallery.tsx
@@ -1,59 +1,70 @@
 import React from "react";
+import {
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+  IconTableColumn,
+} from "@tabler/icons-react";
 import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
-import Gallery1 from "../../images/gallery1.png";
-import Gallery2 from "../../images/gallery2.png";
-import Gallery3 from "../../images/gallery3.png";
-import Gallery4 from "../../images/gallery4.png";
 import Image from "next/image";
 
-type GalleryItem = {
-  className: string;
-  img: string;
-};
+import ImgGallery from "../../images/gallery1.png";
 
 export function FurnitureGallery() {
   return (
-    <BentoGrid className="max-w-7xl mx-auto md:auto-rows-[20rem] px-6">
+    <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
       {items.map((item, i) => (
-        <BentoGridItem key={i} className={item.className}>
-          <Image
-            src={item.img}
-            alt={`Gallery item ${i + 1}`}
-            className="w-full h-full object-cover"
-          />
-        </BentoGridItem>
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          description={item.description}
+          header={item.header}
+          className={item.className}
+          icon={item.icon}
+        />
       ))}
     </BentoGrid>
   );
 }
-
-// const Skeleton: React.FC = () => (
-//   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)] border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black" />
-// );
-
-const items: GalleryItem[] = [
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black">
+    <Image
+      className="w-full h-[350px] object-cover"
+      src={ImgGallery}
+      alt="gal"
+      width={200}
+      height={300}
+    />
+  </div>
+);
+const items = [
   {
-    className: "md:col-span-1",
-    img: Gallery1,
-  },
-  {
-    className: "md:col-span-1",
-    img: Gallery2,
-  },
-  {
+    title: "The Dawn of Innovation",
+    description: "Explore the birth of groundbreaking ideas and inventions.",
+    header: <Skeleton />,
     className: "md:col-span-2",
-    img: Gallery3,
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
   {
+    title: "The Digital Revolution",
+    description: "Dive into the transformative power of technology.",
+    header: <Skeleton />,
+    className: "md:col-span-1",
+    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Art of Design",
+    description: "Discover the beauty of thoughtful and functional design.",
+    header: <Skeleton />,
+    className: "md:col-span-1",
+    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Power of Communication",
+    description:
+      "Understand the impact of effective communication in our lives.",
+    header: <Skeleton />,
     className: "md:col-span-2",
-    img: Gallery4,
-  },
-  {
-    className: "md:col-span-1",
-    img: Gallery3,
-  },
-  {
-    className: "md:col-span-1",
-    img: Gallery2,
+    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
   },
 ];
