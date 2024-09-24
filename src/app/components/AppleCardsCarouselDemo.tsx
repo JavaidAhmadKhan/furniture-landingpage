01697497fbdc1fea1fac3a4ae0f46d7d802814c0
Your Name <relaxjavaid@gmail.com>
2024-09-24 22:26:33 +0530

    fixed apple cards issuessssss

--- src/app/components/AppleCardsCarouselDemo.tsx
+++ src/app/components/AppleCardsCarouselDemo.tsx
@@ -6,10 +6,9 @@ import Slider1 from "../../images/slider1.png";
 import Slider2 from "../../images/slider2.png";
 import Slider3 from "../../images/slider3.png";
 import Slider4 from "../../images/slider4.png";
-import { StaticImageData } from "next/image";
 
 interface CardData {
-  src: StaticImageData;
+  src: string;
 }
 
 // Card data array
