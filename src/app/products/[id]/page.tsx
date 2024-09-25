45aa0c54645732f7cbf9b7a82185e13f0595ec8b
Your Name <relaxjavaid@gmail.com>
2024-09-25 23:10:32 +0530

    added product details page and ts issuessss

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,8 +1,4 @@
-type ProductDetails = {
-  id: string;
-};
-
-export default function page({ params: { id } }: ProductDetails) {
+export default function page({ params: { id } }: string) {
   return (
     <div className="p-6 text-center">
       <h1>Product Id:{id}</h1>
