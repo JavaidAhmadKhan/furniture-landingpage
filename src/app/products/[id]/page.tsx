4761d7aab1b8984915624d9ee3b0cc55f911f3e3
Your Name <relaxjavaid@gmail.com>
2024-09-25 23:05:49 +0530

    added product details page and ts issuessss

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export default function ProductDetails({ params: { id } }) {
+type ProductDetails = {
+  id: string;
+};
+
+export default function ProductDetails({ params: { id } }: ProductDetails) {
   return (
     <div className="p-6 text-center">
       <h1>Product Id:{id}</h1>
