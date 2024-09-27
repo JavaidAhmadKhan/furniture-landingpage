205c5691b52ba2f8ace87c585b2ec30851884dc2
Your Name <relaxjavaid@gmail.com>
2024-09-27 16:39:45 +0530

    add enquire now button in mobile device

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -42,7 +42,7 @@ export default function Page({ params }: Props) {
         <h2 className="text-3xl lg:text-4xl items-start uppercase">
           {product.name}
         </h2>
-        <div>
+        <div className="hidden md:flex">
           <PopupButton />
         </div>
       </div>
@@ -62,9 +62,9 @@ export default function Page({ params }: Props) {
           <p className="text-gray-800 text-2xl lg:text-3xl pt-4 py-6 text-center border-b-2 decoration-dotted border-dashed border-gray-800 border-opacity-15">
             ${product.price}
           </p>
-          <button className="bg-orange-900 hover:bg-orange-950 text-white px-4 py-2 rounded-md flex lg:hidden w-full items-center justify-center my-12">
-            Contact the Seller!
-          </button>
+          <div className=" text-white px-4 py-2 rounded-md flex lg:hidden w-full items-center justify-center my-12">
+            <PopupButton />
+          </div>
         </div>
       </div>
       <div className="pt-6">
@@ -76,38 +76,5 @@ export default function Page({ params }: Props) {
         </p>
       </div>
     </div>
-    // <div className="px-12 py-12 max-w-[100rem] mx-auto min-h-screen">
-    //   <div className="flex justify-between mb-6 lg:mb-12">
-    //     <h2 className="text-3xl lg:text-4xl items-start uppercase">
-    //       {product.name}
-    //     </h2>
-    //   </div>
-
-    //   <div className="flex flex-col lg:flex-row gap-4 lg:gap-6 mb-4">
-    //     <div className="flex items-center justify-center">
-    //       <Image
-    //         className="rounded-lg shadow-xl border-4 border-gray-200 p-2"
-    //         width={600}
-    //         height={600}
-    //         alt={product.name}
-    //         src={product.imageUrl}
-    //       />
-    //     </div>
-    //     <div className="bg-[#E9E4DB] p-6 w-full">
-    //       <label className="font-bold">💰 PRICE:</label>
-    //       <p className="text-gray-800 dark:text-white text-2xl lg:text-3xl pt-4 py-6 text-center border-b-2 decoration-dotted border-dashed border-gray-800 border-opacity-15">
-    //         ₹ {product.price}
-    //       </p>
-    //     </div>
-    //   </div>
-    //   <div className="pt-6">
-    //     <label className="font-bold pb-2 border-b-2 decoration-dotted border-dashed border-gray-800 border-opacity-15">
-    //       📝 DESCRIPTION:
-    //     </label>
-    //     <p className="text-gray-600 dark:text-white text-lg my-4 pt-4 pb-6 ">
-    //       {product.description}
-    //     </p>
-    //   </div>
-    // </div>
   );
 }
