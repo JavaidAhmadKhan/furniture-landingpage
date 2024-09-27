2b6b20b06f898fea63ac2ccb1505610200f7e6f2
Your Name <relaxjavaid@gmail.com>
2024-09-27 16:44:25 +0530

    add font family in product details page

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -39,7 +39,7 @@ export default function Page({ params }: Props) {
   return (
     <div className="px-4 py-12 max-w-7xl mx-auto min-h-screen">
       <div className="flex justify-between mb-6 lg:mb-12">
-        <h2 className="text-3xl lg:text-4xl items-start uppercase">
+        <h2 className="text-3xl font-rubikMedium lg:text-4xl items-start uppercase">
           {product.name}
         </h2>
         <div className="hidden md:flex">
@@ -58,9 +58,9 @@ export default function Page({ params }: Props) {
           />
         </div>
         <div className="bg-[#E9E4DB] p-6 w-full">
-          <label className="font-bold">💰 PRICE:</label>
-          <p className="text-gray-800 text-2xl lg:text-3xl pt-4 py-6 text-center border-b-2 decoration-dotted border-dashed border-gray-800 border-opacity-15">
-            ${product.price}
+          <label className="font-rubikMedium">💰 PRICE:</label>
+          <p className="text-gray-800 text-2xl lg:text-3xl pt-4 py-6 text-center border-b-2 decoration-dotted border-dashed border-gray-800 border-opacity-15 font-rubikMedium">
+            ₹{product.price}
           </p>
           <div className=" text-white px-4 py-2 rounded-md flex lg:hidden w-full items-center justify-center my-12">
             <PopupButton />
@@ -68,10 +68,10 @@ export default function Page({ params }: Props) {
         </div>
       </div>
       <div className="pt-6">
-        <label className="font-bold pb-2 border-b-2 decoration-dotted border-dashed border-gray-800 border-opacity-15">
+        <label className="font-bold pb-2 font-rubikMedium border-b-2 decoration-dotted border-dashed border-gray-800 border-opacity-15">
           📝 DESCRIPTION:
         </label>
-        <p className="text-gray-600 text-lg my-4 pt-4 pb-6 ">
+        <p className="text-gray-600 text-lg my-4 pt-4 pb-6 rubik ">
           {product.description}
         </p>
       </div>
